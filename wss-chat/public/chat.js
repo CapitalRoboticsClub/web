ccc159b36cf785a505068dc51eb520cdbe8bba73
yunliyun <46965053+yunliyun@users.noreply.github.com>
2021-06-19 16:24:23 -0400

    test node

--- wss-chat/public/chat.js
+++ wss-chat/public/chat.js
@@ -1,6 +1,6 @@
 //make connection with server
 //var socket = io.connect('http://localhost:8000'); //localhost is local to the device where client is on. 
-var socket = io.connect('http://54.227.123.162:8000');
+var socket = io.connect('http://54.227.123.162:8000/');
 
 //query DOM
 var message = document.getElementById('message'),
