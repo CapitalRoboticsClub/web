bee8778bb764c91865e47646606d84da5e33bfc1
yunliyun <46965053+yunliyun@users.noreply.github.com>
2021-06-19 01:17:49 -0400

    test

--- wss-chat/client.js
+++ wss-chat/client.js
@@ -1,4 +1,4 @@
-const connection = new WebSocket("ws://' + location.host + '/ws');
+const connection = new WebSocket('ws://' + location.host + '/ws');
 const button = document.querySelector("#send");
 
 connection.onopen = (event) => {
